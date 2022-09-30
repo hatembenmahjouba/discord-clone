f2d20d406b4b9a7e13d2d66cf3a9d754e34a23ae
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-09-30 14:05:33 +0200

    adding friends list

--- src/components/FriendsSideBar/FriendsList/FriendsList.js
+++ src/components/FriendsSideBar/FriendsList/FriendsList.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import { styled } from '@mui/system';
+import FriendsListItem from './FriendsListItem';
+
+const DUMMY_FRIENDS = [
+  {
+    id: 1,
+    username: 'Mark',
+    isOnline: true,
+  },
+  {
+    id: 2,
+    username: 'Anna',
+    isOnline: true,
+  },
+  {
+    id: 3,
+    username: 'John',
+    isOnline: false,
+  },
+];
 
 const MainContainer = styled('div')({
   flexGrow: 1,
-  width: '100',
+  width: '100%',
 });
 
 const FriendsList = () => {
-  return <MainContainer></MainContainer>;
+  return (
+    <MainContainer>
+      {DUMMY_FRIENDS.map((friend) => (
+        <FriendsListItem
+          username={friend.username}
+          id={friend.id}
+          key={friend.id}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </MainContainer>
+  );
 };
 
 export default FriendsList;
