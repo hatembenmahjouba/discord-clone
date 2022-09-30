f07775e535074ff2eec8a3ceae2767d89ace0306
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-09-30 18:06:13 +0200

    creating dropdownmenu with logout functionality and checking if token exist on Dashboard Page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { styled } from '@mui/system';
 import SideBar from '../components/SideBar/SideBar';
 import FriendsSideBar from '../components/FriendsSideBar/FriendsSideBar';
 import Messenger from '../components/Messenger/Messenger';
 import AppBar from '../components/AppBar/AppBar';
+import { logout } from '../utils/auth';
+import { getActions } from '../redux/actions/authActions';
+import { useDispatch } from 'react-redux';
 
 const Wrapper = styled('div')({
   width: '100%',
@@ -12,6 +15,16 @@ const Wrapper = styled('div')({
 });
 
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const userDetails = localStorage.getItem('user');
+    if (!userDetails) {
+      logout();
+    } else {
+      const { setUserDetails } = getActions(dispatch);
+      setUserDetails(JSON.parse(userDetails));
+    }
+  }, [dispatch]);
   return (
     <Wrapper>
       <SideBar />
